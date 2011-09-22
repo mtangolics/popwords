086c4c5741dcc72edd7acbbcc7dea2a2e3d952b8
Mike Tangolics <mtangolics@gmail.com>
2011-09-22 19:25:28 +0000

    fixed copy/paste mistake

--- ui/years.php
+++ ui/years.php
@@ -1,16 +1,10 @@
 <?php 
     require_once("../awsphp/sdk.class.php");
 	
-    if($year != "") 
-    {
-    	$sdb = new AmazonSDB();
-    	$resp = $sdb->domain_metadata("poplyrics");
-    	
-    	$attr = $resp->body;
-    	echo $attr;
-    }
-    else
-    {
-        echo "{}";
-    }
+	$sdb = new AmazonSDB();
+	$resp = $sdb->domain_metadata("poplyrics");
+	
+	$attr = $resp->body;
+	echo $attr;
+
 ?>
