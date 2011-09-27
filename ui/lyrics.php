dd9d876609cef79951f4e9840c7793daf2d6f401
Mike Tangolics <mtangolics@gmail.com>
2011-09-27 04:59:43 -0400

    Removed debugging from ui

--- ui/lyrics.php
+++ ui/lyrics.php
@@ -7,9 +7,7 @@
     {
     	$sdb = new AmazonSDB();
     	$resp = $sdb->get_attributes("poplyrics",$year);
-    	echo $year;
     	$attr = $resp->body->GetAttributesResult[0];
-		echo $resp->status;
 		if($attr) {
 			echo $attr->to_json();
 		}
